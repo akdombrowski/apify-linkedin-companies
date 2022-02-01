85c6f7cf33de351881b26cc2d9ca8cd5b4e7513f
akdombrowski <akdflyinvee73@gmail.com>
2022-01-31 19:02:14 -0600

    add puppeteer import puppeteer starter code

--- main.js
+++ main.js
@@ -2,25 +2,37 @@
 // It is referenced from the "scripts" section of the package.json file,
 // so that it can be started by running "npm start".
 
-const Apify = require('apify');
+// Import Apify SDK. For more information, see https://sdk.apify.com/
+const Apify = require("apify");
 
 Apify.main(async () => {
-    // Get input of the actor.
-    // If you'd like to have your input checked and generate a user interface
-    // for it, add INPUT_SCHEMA.json file to your actor.
-    // For more information, see https://apify.com/docs/actor/input-schema
-    const input = await Apify.getInput();
-    console.log('Input:');
-    console.dir(input);
+  // Get input of the actor (here only for demonstration purposes).
+  // If you'd like to have your input checked and have Apify display
+  // a user interface for it, add INPUT_SCHEMA.json file to your actor.
+  // For more information, see https://docs.apify.com/actors/development/input-schema
+  const input = await Apify.getInput();
+  console.log("Input:");
+  console.dir(input);
 
-    // Do something useful here...
+  if (!input || !input.url)
+    throw new Error('Input must be a JSON object with the "url" field!');
 
-    // Save output
-    const output = {
-        receivedInput: input,
-        message: 'Hello sir!',
-    };
-    console.log('Output:');
-    console.dir(output);
-    await Apify.setValue('OUTPUT', output);
+  console.log("Launching Puppeteer...");
+  const browser = await Apify.launchPuppeteer();
+
+  console.log(`Opening page ${input.url}...`);
+  const page = await browser.newPage();
+  await page.goto(input.url);
+  const title = await page.title();
+  console.log(`Title of the page "${input.url}" is "${title}".`);
+
+  console.log("Saving output...");
+  await Apify.setValue("OUTPUT", {
+    title,
+  });
+
+  console.log("Closing Puppeteer...");
+  await browser.close();
+
+  console.log("Done.");
 });
